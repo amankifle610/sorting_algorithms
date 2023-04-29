7bcc553943553e6278e8f20bca45f77f83e5f738
amankifle610 <amankifle610@gmail.com>
2023-04-29 06:39:10 -0700

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,19 +6,19 @@
 */
 void bubble_sort(int *array, size_t size)
 {
-	int i = 0, j = 0, tmp;
+	size_t i = 0, j = 0, tmp = 0;
 
 	if (size < 2)
 		return;
-	for (i = 0; i < (size - 1); i++)
+	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
 		{
-			if (array[j - 1] > array[j])
+			if (array[j] > array[j + 1] && array[j + 1])
 			{
 				tmp = array[j];
-				array[j] = array[j - 1];
-				array[j - 1] = tmp;
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
 				print_array(array, size);
 			}
 		}
