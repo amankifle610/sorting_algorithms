09b1cee57457c5bdfeb12e459e79fb1a7f6998b6
amankifle610 <amankifle610@gmail.com>
2023-04-29 06:06:22 -0700

    0-bubble_sort.c

--- sort.h
+++ sort.h
@@ -12,7 +12,7 @@ typedef struct listint_s
 	const int n;
 	struct listint_s *prev;
 	struct listint_s *next;
-}listint_t;
+} listint_t;
 void bubble_sort(int *array, size_t size);
 void print_rray(const int *array, size_t size);
 void print_list(const listint_t *list);
